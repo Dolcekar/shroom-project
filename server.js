44ce75ae64bb35e813b60851ca4a17e02bd390d2
Carla Cox <astronomykar@gmail.com>
2018-08-25 12:55:10 -0400

    ath-integrated

--- server.js
+++ server.js
@@ -2,18 +2,58 @@ require("dotenv").config();
 var express = require("express");
 var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
+var createError = require('http-errors');
+var path = require('path');
+var logger = require('morgan');
+var session = require('express-session');
+const usersRouter = require("./routes/users");
+var okta = require("@okta/okta-sdk-nodejs");
+var ExpressOIDC = require("@okta/oidc-middleware").ExpressOIDC;
 
+const dashboardRouter = require("./routes/dashboard");        
+const publicRouter = require("./routes/public");
 var db = require("./models");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+function loginRequired(req, res, next) {
+  if (!req.user) {
+    return res.status(401).render("unauthenticated");
+  }
+ 
+  next();
+}
+
+
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
+var oktaClient = new okta.Client({
+  orgUrl: 'https://dev-942199.oktapreview.com',
+  token: '00Ikm9L34b_q8bLIdFk4l-yT1wQbwMrtK1Ba8FzO8e'
+});
+//some problems in session. Maybe syntax? Refer to documentation
+const oidc = new ExpressOIDC({
+  issuer: "https://dev-942199.oktapreview.com/oauth2/default",
+  client_id: "0oag0t64ob2cWyDH50h7",
+  client_secret: "7Czen6EEUgu7TMirHgIKblPilwARFX2DmJs8eiyD",
+  redirect_uri: 'http://localhost:3000/users/callback',
+  scope: "openid profile",
+  routes: {
+    login: {
+      path: "/users/login"
+    },
+    callback: {
+      path: "/users/callback",
+      defaultRedirect: "/dashboard"
+    }
+  }
+});
 
-// Handlebars
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
 app.engine(
   "handlebars",
   exphbs({
@@ -21,6 +61,52 @@ app.engine(
   })
 );
 app.set("view engine", "handlebars");
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'asdf;lkjh3lkjh235l23h5l235kjh',
+  resave: true,
+  saveUninitialized: false
+}));
+app.use(oidc.router);
+app.use((req, res, next) => {
+  if (!req.userinfo) {
+    return next();
+  }
+ 
+  oktaClient.getUser(req.userinfo.sub)
+    .then(user => {
+      req.user = user;
+      res.locals.user = user;
+      next();
+    }).catch(err => {
+      next(err);
+    });
+});
+
+app.use('/', publicRouter);
+app.use('/dashboard', loginRequired, dashboardRouter);
+app.use('/users', usersRouter);
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
 
 // Routes
 require("./routes/apiRoutes")(app);
